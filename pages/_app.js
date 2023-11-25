17d0f3020f27fe52134d9627fe4dfec005cf08a2
Zach Herring <2172549+zherring@users.noreply.github.com>
2023-11-25 13:10:32 -0700

    commiting gitignore

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// pages/_app.js
-import '/styles/globals.css'; // Adjust the path according to your project structure
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
